815ebfa843b53d568f3b7971605e1b3624442276
realdennis <realdennis777@gmail.com>
2020-11-22 19:48:51 +0800

    no devtool verify

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,4 +1,8 @@
-export default ()=>{
-    // I am a hello world function
-    console.log("Hello world");
-}
+export default () => {
+  // I am a hello world function
+  console.log("Hello world");
+
+  // Let mobile user verify it
+  const messageEl = document.querySelector(".message");
+  messageEl instanceof HTMLElement && (messageEl.innerText = "Hello world :D");
+};
